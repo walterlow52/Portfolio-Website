7a21f2c05ac9510b4c5d636cec7fa132261c0e00
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2023-06-21 11:00:09 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,9 +10,9 @@ document.querySelector("#brand").onmouseover = e => {
         return e.target.dataset.value(i);
       }
       return letters[Math.floor(Math.random() * 26)]}).join("");
-    /*if (iteration >= e.target.dataset.value.length) {
+    if (iteration >= e.target.dataset.value.length) {
       clearInterval(interval);
-    }*/
+    }
     iteration += 1/3;
   }, 30);
 }
