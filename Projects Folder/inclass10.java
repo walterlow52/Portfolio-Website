26d51eb4007763fba4387fb201e6f066ba198f41
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2023-07-04 19:04:51 -0400

    Add files via upload

---
+++ Projects Folder/inclass10.java
@@ -0,0 +1,23 @@
+package projects;
+import java.util.Scanner;
+
+public class inclass10 {
+
+	public static void main(String[] args) {
+		// declare variables
+		Scanner keybd = new Scanner(System.in);
+		System.out.print("This program analyzes if a given integer is positive, negative, or a zero number.\nEnter an integer: ");
+		int integer = keybd.nextInt();
+				
+		if (integer > 0)
+		System.out.print("positive");
+		
+		else if (integer == 0)
+		System.out.print("zero");
+		
+		else if (integer < 0)
+		System.out.print("negative");
+
+	}
+
+}
