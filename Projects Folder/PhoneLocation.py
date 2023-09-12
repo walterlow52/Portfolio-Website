d584c9435406ebe121d9590b61384721976f141e
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2023-09-12 16:40:20 -0400

    Delete Projects Folder/PhoneLocation.py

--- Projects Folder/PhoneLocation.py
+++
@@ -1,5 +0,0 @@
-import phonenumbers
-from phonenumbers import geocoder
-num1 = phonenumbers.parse("+13474561326")
-print("\nPhone Numbers Location")
-print(geocoder.description_for_number(num1, "en"))
